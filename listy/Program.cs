e91973e25bf593cbe14656b227334301681b61b1
Tomasz Fryckowski <tomaszfryckowski@gmail.com>
2023-10-03 17:40:58 +0200

    Dodaj pliki projektów.

---
+++ listy/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Markup;
+
+class Egg
+{
+    public void Main(string[] args)
+    {
+
+        List<Egg> MyCarton = new List<Egg>(); //tworzenie obiektu
+
+        Egg x = new Egg(); //dodawanie do listy
+        MyCarton.Add(x);
+
+        Egg y = new Egg(); // dodawanie do listy czegoś innego
+        MyCarton.Add(y);
+
+        int size = MyCarton.Count; // sprawdzanie elementów znajdujących się na liście
+
+        bool isIn = MyCarton.Contains(x); //sprawdzanie czy lista zawiera określony element
+
+        int index = MyCarton.IndexOf(x); // sprawdzanie gdzie na liście znajduje się określony elemnt
+
+        MyCarton.Remove(x); // usuwanie z listy
+    }
+
+  
+
+
+
+}
